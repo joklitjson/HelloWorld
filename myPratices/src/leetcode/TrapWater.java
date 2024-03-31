e7eb3887c075ca0acfb700562fc57bbdb56c097e
wenlong.chen <wenlong.chen@weimob.com>
2024-03-31 11:43:55 +0800

    1

--- myPratices/src/leetcode/TrapWater.java
+++ myPratices/src/leetcode/TrapWater.java
@@ -1,25 +1,12 @@
-package basestructs;
+package leetcode;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 
-public class DequeTest {
+public class TrapWater {
     public static void main(String[] args) {
-        Deque<Integer> stringDeque=new LinkedList<>();
 
-        stringDeque.addFirst(2);
-        stringDeque.addFirst(5);
-        stringDeque.addFirst(6);
-
-        System.out.println(stringDeque.element());
-        System.out.println(stringDeque);
-
-        stringDeque.offerLast(77);
-
-        stringDeque.offerLast(88);
-
-        System.out.println(stringDeque);
         int [] arr=new int[]{4,2,0,3,2,5};
         System.out.println(trap(arr));
         System.out.println(trap2(arr));
@@ -30,16 +17,16 @@ public class DequeTest {
         Deque<Integer> deque=new ArrayDeque<>();
         for (int i=0;i<height.length;i++){
             while (!deque.isEmpty()&&height[i]>height[deque.peek()]){
-                 int top= deque.pop();
-                 //左边没元素了，所以不能接水
-                 if (deque.isEmpty()){
+                int top= deque.pop();
+                //左边没元素了，所以不能接水
+                if (deque.isEmpty()){
                     continue;
-                 }
-                 int left=deque.peek();
-                 int width=i-left-1;
+                }
+                int left=deque.peek();
+                int width=i-left-1;
 //                 计算高度，左右两边的最小值，然后减去中间元素 的高度
-                 int minHeight=Math.min(height[left],height[i])-height[top];
-                 ans+=width*minHeight;
+                int minHeight=Math.min(height[left],height[i])-height[top];
+                ans+=width*minHeight;
             }
             deque.push(i);
         }
@@ -72,4 +59,5 @@ public class DequeTest {
         }
         return  ans;
     }
+
 }
