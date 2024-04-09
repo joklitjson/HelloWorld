60d0c9325826cca24183cf147bf37a5f37e6a9bf
wenlong.chen <wenlong.chen@weimob.com>
2024-04-09 09:20:47 +0800

    更新

--- myPratices/src/leetcode/SliceWindowTest.java
+++ myPratices/src/leetcode/SliceWindowTest.java
@@ -169,7 +169,8 @@ public class SliceWindowTest {
         int left = 0, right = 0;
         int maxLongst = 0;
         for (; right < s.length(); right++) {
-            if (map.containsKey(s.charAt(right))) {
+            //必须要大于左指针,优先排除无效指针数据
+            if (map.containsKey(s.charAt(right))&&map.get(s.charAt(right))>=left) {
 //原则上 map.getOrDefault(s.charAt(right),0)+1是最大值，但是也有可能历史中有此数据，
 // 因此会有误判的情况，所以需要比较下left和当前left值的最大值。主要原因还是由于map清空历史数据的问题
                 left = Math.max(left, map.getOrDefault(s.charAt(right), 0) + 1);
