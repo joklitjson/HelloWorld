6026d1c338e8d4f93e25cf1f838a6572ea59d8b8
wenlong.chen <wenlong.chen@weimob.com>
2024-05-17 16:55:30 +0800

    开锁问题，使用BFS扩散解决

--- myPratices/src/tree/BFSTemplete.java
+++ myPratices/src/tree/BFSTemplete.java
@@ -51,4 +51,76 @@ public class BFSTemplete {
 
         return -1;
     }
+
+    public static void main(String[] args) {
+
+    }
+    public int openLock(String[] deadends, String target){
+        String start="0000";
+        Queue<String> queue=new LinkedList<>();
+        Set<String> visited=new HashSet<>();//是否已经访问
+        Set<String> deadendsSet=new HashSet<>();
+        for (String de:deadends){
+            deadendsSet.add(de);
+        }
+
+        queue.add(start);
+        visited.add(start);
+        int step=0;
+        while (!queue.isEmpty()){
+            int  count=queue.size();
+            for (int i=0;i<count;i++){
+                String current= queue.poll();
+                //判断是否等于目标
+                if (target.equals(current)){
+                    return step;
+                }
+                //如果包含了这个，那就跳过他的扩展
+                if (deadendsSet.contains(current)){
+                    continue;
+                }
+                //然后再四个位置上，向上或向下变换
+                for (int j=0;j<4;j++){
+                    String plusOn=plusOne(current,j);
+                    if (!visited.contains(plusOn)){
+                        queue.offer(plusOn);
+                        visited.add(plusOn);
+                    }
+                    String subOn=subOne(current,j);
+                    if (!visited.contains(subOn)){
+                        queue.add(subOn);
+                        visited.add(subOn);
+                    }
+                }
+            }
+            /* 在这里增加步数 */
+            step++;
+        }
+//如果找不到，那就直接返回-1
+        return -1;
+    }
+
+    private static String plusOne(String orign,int index){
+        char[] chars= orign.toCharArray();
+        if (chars[index]=='9'){
+            chars[index]='0';
+        }
+        else {
+            chars[index]+=1;
+        }
+
+        return new String(chars);
+    }
+
+    private static String subOne(String orign,int index){
+        char[] chars= orign.toCharArray();
+        if (chars[index]=='0'){
+            chars[index]='9';
+        }
+        else {
+            chars[index]-=1;
+        }
+
+        return new String(chars);
+    }
 }
