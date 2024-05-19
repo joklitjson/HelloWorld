971316f6e2acf593e4ada6236205b26de4daeb54
wenlong.chen <wenlong.chen@weimob.com>
2024-05-19 09:45:13 +0800

    优化归并排序合并方法

--- myPratices/src/sort/MergeSort.java
+++ myPratices/src/sort/MergeSort.java
@@ -1,8 +1,11 @@
 package sort;
 
 public class MergeSort {
+
+    static int []tmp=null;
     public static void main(String[] args) {
         int[] arr=new int[]{1, 31, 4, 1, 2, 5, 7, 8,};
+        tmp=new int[arr.length];
         mergeSort(arr,0,arr.length-1);
 
         for (int value : arr) {
@@ -22,36 +25,55 @@ public class MergeSort {
 
     /**
      * 合并两个数组
+     * 合并两个数组
      * @param arr
      * @param left
      * @param middle
      * @param right
      */
     private static void merge(int [] arr,int left,int middle, int right){
-        int tmp[]=new int[right-left+1];
-        int i=left;
-        int j=middle+1;
-        int index=0;
 
-        //合并两个数组
-        while (i<=middle||j<=right){
-            if (i<=middle&&j<=right){
-                tmp[index++]=arr[i]<arr[j]?arr[i++]:arr[j++];
-            }
-            else {
-                if (i <= middle) {
-                    tmp[index++] = arr[i++];
-                }
-                if (j <= right) {
-                    tmp[index++] = arr[j++];
-                }
+        //copy排好的数组
+        for(int i=left;i<=right;i++){
+            tmp[i]=arr[i];
+        }
+        int i=left,p=left,j=middle+1;
+        while (p<=right) {
+            if (i == middle + 1) {
+                //左半部分的数据已经用完
+                arr[p++] = tmp[j++];
+            } else if (j == right + 1) {
+                arr[p++] = tmp[i++];
+            } else if (tmp[i] < tmp[j]) {
+                arr[p++] = tmp[i++];
+            } else {
+                arr[p++] = tmp[j++];
             }
         }
-
-        index=left;
-        //填充到原数组
-        for (int value:tmp){
-            arr[index++]=value;
-        }
+//        int tmp[]=new int[right-left+1];
+//        int i=left;
+//        int j=middle+1;
+//        int index=0;
+//
+//        //合并两个数组
+//        while (i<=middle||j<=right){
+//            if (i<=middle&&j<=right){
+//                tmp[index++]=arr[i]<arr[j]?arr[i++]:arr[j++];
+//            }
+//            else {
+//                if (i <= middle) {
+//                    tmp[index++] = arr[i++];
+//                }
+//                if (j <= right) {
+//                    tmp[index++] = arr[j++];
+//                }
+//            }
+//        }
+//
+//        index=left;
+//        //填充到原数组
+//        for (int value:tmp){
+//            arr[index++]=value;
+//        }
     }
 }
