f09c3e6396a7f4dd5f79624376e2cf0084fd2098
wenlong.chen <wenlong.chen@weimob.com>
2024-06-04 21:35:31 +0800

    二分图分人群

--- myPratices/src/datastrucs/BipartiteCheck.java
+++ myPratices/src/datastrucs/BipartiteCheck.java
@@ -2,6 +2,7 @@ package datastrucs;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BipartiteCheck {
 
@@ -25,6 +26,47 @@ public class BipartiteCheck {
         return  isOk;
     }
 
+    boolean isBipartite2(int[][] graph){
+        visited=new boolean[graph.length];
+        boolean [] color=new boolean[graph.length];//染色
+        Queue<Integer> queue=new LinkedList<>();
+        queue.add(0);
+        visited[0]=true;
+
+        for (int v=0;v<graph.length;v++){
+            if (isOk){
+                bfs(graph,color,v);
+            }
+        }
+        return  isOk;
+    }
+
+    /**
+     * 广度遍历
+     * @param graph
+     * @param color
+     * @param element
+     */
+    void bfs(int[][] graph, boolean [] color,int element) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(element);
+        visited[element] = true;
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            for (int neight : graph[curr]) {
+                if (visited[neight]) {
+                    if (color[neight] == visited[curr]) {
+                        isOk = false;
+                        break;
+                    }
+                } else {
+                    color[neight] = !visited[curr];
+                    queue.offer(neight);
+                    visited[neight] = true;
+                }
+            }
+        }
+    }
     void traverse(int[][] graph, boolean [] color,int element){
         if (!isOk){
             return;
@@ -50,22 +92,55 @@ public class BipartiteCheck {
         }
     }
 
-    private List<Integer> [] buildGraph(int[][] graph){
-        int n=graph.length;
+    private List<Integer> [] buildGraph(int n,int[][] graph){
         List<Integer> [] graphs=new LinkedList[n];
 
         //初始化邻接表
         for (int i=0;i<n;i++){
             graphs[i]=new LinkedList<>();
         }
-
         for (int[] edge:graph){
             //无向图 就是添加两个边
             graphs[edge[0]].add(edge[1]);
             graphs[edge[1]].add(edge[0]);
         }
-
         return graphs;
-
     }
+
+    /**
+     * 把互相讨厌的两个人 分成两个小组,实际就是二分图
+     * @param n
+     * @param dislikes
+     * @return
+     */
+    boolean possibleBipartition(int n, int[][] dislikes) {
+        visited = new boolean[n + 1];
+        boolean[] color = new boolean[n + 1];
+
+        List<Integer>[] list = buildGraph(n, dislikes);
+        for (int i = 1; i <= n; i++) {
+            travese(list, color, i);
+        }
+        return isOk;
+    }
+
+    private void  travese(List<Integer> [] graph,boolean [] color,int element){
+        if (visited[element]){
+            return;
+        }
+        visited[element]=true;
+        for (int neight:graph[element]){
+            if (visited[neight]){
+                if (color[neight]==color[element]){
+                    isOk=false;
+                    break;
+                }
+            }
+            else {
+                color[neight]=!color[element];
+                travese(graph,color,neight);
+            }
+        }
+    }
+
 }
