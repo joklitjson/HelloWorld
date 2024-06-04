0cd7ed2cb97e42fada60ed800dc3069f523aa9ea
wenlong.chen <wenlong.chen@weimob.com>
2024-06-04 17:52:42 +0800

    新增二分图的判断以及联通图的判断

--- myPratices/src/datastrucs/BipartiteCheck.java
+++ myPratices/src/datastrucs/BipartiteCheck.java
@@ -11,13 +11,50 @@ public class BipartiteCheck {
      * @param graph
      * @return
      */
+    boolean [] visited;
+    boolean isOk=true;
     boolean isBipartite(int[][] graph){
-        return  true;
+        visited=new boolean[graph.length];
+        boolean [] color=new boolean[graph.length];//染色
+        for (int i=0;i<graph.length;i++){
+            if (!isOk){
+                return isOk;
+            }
+            traverse(graph,color,i);
+        }
+        return  isOk;
+    }
+
+    void traverse(int[][] graph, boolean [] color,int element){
+        if (!isOk){
+            return;
+        }
+        if (visited[element]){
+            return;
+        }
+        visited[element]=true;
+        for (int neight:graph[element]){
+            if (visited[neight]){
+                //已经设置了颜色：判断他是否和element的颜色相同
+                if (color[neight]==color[element]){
+                    isOk=false;
+                    return;
+                }
+                continue;
+            }
+            else {
+                //未设置颜色，则测试不同的颜色
+                color[neight]=!color[element];
+                traverse(graph,color,neight);
+            }
+        }
     }
 
     private List<Integer> [] buildGraph(int[][] graph){
         int n=graph.length;
         List<Integer> [] graphs=new LinkedList[n];
+
+        //初始化邻接表
         for (int i=0;i<n;i++){
             graphs[i]=new LinkedList<>();
         }
---
+++ myPratices/src/datastrucs/UF.java
@@ -0,0 +1,76 @@
+package datastrucs;
+
+/**
+ * 连通图相关的用例
+ */
+public class UF {
+
+    /**
+     * 联通分量
+     */
+    private int count;
+    //存放元素的父节点
+    int [] parent;
+
+    public UF(int count){
+        this.count=count;
+        parent=new int[count];
+
+        //设置每一个节点的父节点指向自己
+        for (int i=0;i<count;i++){
+            parent[i]=i;
+        }
+    }
+
+    /**
+     * 判断两个节点是否联通
+     * @param a
+     * @param b
+     * @return
+     */
+    public boolean connect(int a,int b){
+        int parentA=findParent(a);
+        int parentB=findParent(b);
+        return parentA==parentB;
+    }
+
+    /**
+     * 联通a和b
+     * @param a
+     * @param b
+     */
+    public void union(int a,int b){
+        int parentA=findParent(a);
+
+        int parentB=findParent(b);
+        //随便把他指向一个节点
+        if (parentA!=parentB){
+          parent[parentA]=parentB;
+          count--;
+        }
+    }
+
+    /**
+     * 查找a的父节点s
+     * @param x
+     * @return
+     */
+    public int findParent(int x){
+
+        //会造成数据变成链表一样，因此我们需要把他进行压缩
+//        while (x!=parent[x]){
+//            x=parent[x];
+//        }
+//        return x;
+
+        //2、压缩版
+        while (x!=parent[x]){
+            //把当前节点的父节点指向他的爷爷
+            parent[x]=parent[parent[x]];
+            x=parent[x];
+        }
+
+        return x;
+    }
+
+}
