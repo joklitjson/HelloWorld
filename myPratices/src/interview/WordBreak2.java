04f1a1e5d93f81b2211158c8677e420b5dcfa7b8
wenlong.chen <wenlong.chen@weimob.com>
2024-07-13 12:37:54 +0800

    单词拆分 返回路径

--- myPratices/src/interview/WordBreak2.java
+++ myPratices/src/interview/WordBreak2.java
@@ -1,8 +1,6 @@
 package interview;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * 单词拆分，就是 能否使用一个集合中的单词组成一个目标字符串
@@ -10,21 +8,36 @@ import java.util.List;
  * 1、回溯算法：使用组合的方式遍历
  * 2、动态规划
  */
-public class WordBreak {
+public class WordBreak2 {
    static   HashSet<String> wordSet=new HashSet<>();
 
-   static int [] memo;
+  static List<String> res=new ArrayList<>();
+   static List<String> [] memo;
     public static void main(String[] args) {
 
 //        System.out.println(wordBreak("hellow", Arrays.asList("ll","he","ow")));
 //        System.out.println(wordBreak("leetcode", Arrays.asList("le", "code","et")));
-        System.out.println(wordBreak("a", Arrays.asList("b")));
+//        List<String> result= wordBreak("leetcode", Arrays.asList("le", "code","et"));
+        List<String> result= wordBreak("catsanddog", Arrays.asList("cat","cats","and","sand","dog"));
+        System.out.println("target string=[catsanddog]");
+        for (String ans:result){
+            System.out.println(ans);
+        }
+//        System.out.println(wordBreak("catsanddog", Arrays.asList("cat","cats","and","sand","dog")));
     }
-   static boolean  wordBreak(String s, List<String> wordDict) {
+
+    /**
+     * 需要返回拆解的路径
+     * @param s
+     * @param wordDict
+     * @return
+     */
+   static List<String>  wordBreak(String s, List<String> wordDict) {
        wordSet.addAll(wordDict);
+       res.clear();
+
        //是被备忘录
-       memo = new int[s.length() + 1];
-       Arrays.fill(memo,-1);
+       memo = new List[s.length() + 1];
 //       return backtrack(s, 0, wordDict);
        return dp(s,0);
    }
@@ -60,30 +73,36 @@ public class WordBreak {
      * 定义dp[i] 函数：i...n是否匹配，如果 0，i被匹配，同时子问题 i...n也能被匹配，则肯定能拼接
      * @return
      */
-    private static boolean dp(String s,int index) {
+    private static  List<String>  dp(String s,int index) {
+        List<String> res = new LinkedList<>();
         if (index == s.length()) {
-            return true;
+            res.add("");
+            return res;
         }
 
-        if (memo[index]!=-1){
-            return memo[index]>0;
+        if (memo[index]!=null) {
+            return memo[index];
         }
 
         for (int i = index+1; i <= s.length(); i++) {
             // 0..i 被匹配，需要看看子问题
-            if (wordSet.contains(s.substring(index, i))) {
+            String prefix = s.substring(index, i);
 
+            if (wordSet.contains(prefix)) {
                 //子问题也被匹配到了
-                boolean subProm = dp(s, i);
-                if (subProm) {
-                    memo[i] = 1;
-                    return true;
+                List<String> subPromPath = dp(s, i);
+                for (String sub : subPromPath) {
+                    if (sub.isEmpty()) {
+                        res.add(prefix);
+                    } else {
+                        res.add(prefix + " " + sub);
+                    }
                 }
             }
         }
         // s[i..] 无法被拼出
-        memo[index] = 0;
+        memo[index] =res;
         //遍历完成，都不能被匹配
-        return false;
+        return res;
     }
 }
