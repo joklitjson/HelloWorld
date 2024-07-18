cecfad2fc64fb6a1a94140385d32b4453db4d4ae
wenlong.chen <wenlong.chen@weimob.com>
2024-07-18 23:10:04 +0800

    二叉树的前序迭代遍历的两种写法

--- myPratices/src/tree/TreePreorderTraverse.java
+++ myPratices/src/tree/TreePreorderTraverse.java
@@ -4,9 +4,12 @@ import datastrucs.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 
 /**
+ *
+ * https://mp.weixin.qq.com/s?__biz=MzAxMjY5NDU2Ng==&mid=2651862003&idx=3&sn=d50a4e5bde1cd4c3b285f3247f2fd332&chksm=804976bab73effacd8c0f76646d6d9a30489239742ba7dedc90f9a54d37b7562426dd93321b1&scene=27
  * 前序位置的代码执行是自顶向下的，而后序位置的代码执行是自底向上的：
  *
  * 前序位置是刚刚进入节点的时刻，后序位置是即将离开节点的时刻。
@@ -34,6 +37,57 @@ public class TreePreorderTraverse {
         result.addAll(preorderTraverse(treeNode.getLeft()));
         //遍历右子树，获取右子树的
         result.addAll(preorderTraverse(treeNode.getRight()));
+        return result;
+    }
+
+    //前序遍历 迭代写法,借助栈
+    public  List<Integer>  preOrderIterator(TreeNode treeNode) {
+        List<Integer> result = new ArrayList<>();
+        if (treeNode == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+
+        stack.push(treeNode);
+        while (!stack.isEmpty()) {
+            //取出根节点
+            TreeNode root = stack.pop();
+            result.add(root.val);
+            //把左右子数都加入到栈中，因为下一次需要先取出左子数遍历，因此需要先把右子树加入到栈中
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+
+            if (root.left != null) {
+                stack.push(root.left);
+            }
+        }
+        return result;
+    }
+
+    //迭代的第二种写法
+    public  List<Integer>  preOrderIterator2(TreeNode treeNode) {
+
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = treeNode;
+
+        while (!stack.isEmpty() || cur != null) {
+
+            while (cur != null) {
+                stack.push(cur);
+                /************************/
+                //此处就是先访问的根节点
+                result.add(cur.val);
+                //访问左子树
+                cur = cur.left;
+            }
+            //把根节点弹出
+            cur = stack.pop();
+
+            //准备便利右子树
+            cur = cur.right;
+        }
         return result;
     }
 }
