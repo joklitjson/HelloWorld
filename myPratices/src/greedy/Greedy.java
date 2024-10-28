3dde09aeb6eb6c704dddf006f262ac39fa321c09
wenlong.chen <wenlong.chen@weimob.com>
2024-10-28 16:29:51 +0800

    update

--- myPratices/src/greedy/Greedy.java
+++ myPratices/src/greedy/Greedy.java
@@ -1,6 +1,7 @@
 package greedy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Greedy {
@@ -84,4 +85,54 @@ public class Greedy {
         }
         return total >= 0 ? ans : -1;
     }
+
+    /**
+     * 611. 有效三角形的个数
+     * 方案1：先排序，然后在循环数组，定义为i，然后在第二个循环中定义j=i+1， 在 j,n的范围内 二分查找 arr[i]+arr[j] 的最小左边界k,然后 n-k 就是有效的数量
+     * 方案二：排序+双指针 类似方案一，我们可以把他取名叫做三指针问题,先排序，然后在进行两次循环，第二个数和第三个数 我们使用指针查找，一直找到合适的数字
+     *
+     * @param nums
+     * @return
+     */
+    public int triangleNumber(int[] nums) {
+
+        Arrays.sort(nums);
+        int n=nums.length;
+        int ans=0;
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<n;j++){
+                //最左侧二分查找
+                int left=j+1,right=n-1, k=left;
+                while (left<=right){
+                    int middle=(left+right)/2;
+                    if (nums[middle]<nums[i]+nums[j]){
+                        k=middle;
+                        left=middle+1;
+                    }
+                    else {
+                        right=middle-1;
+                    }
+                }
+                //这区间的都是答案
+                ans+=k-j;
+            }
+        }
+
+        //排序+双指针
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<n;j++) {
+                int k = j + 1;
+                //寻找到满足条件的k
+                while (k + 1 < n && nums[k + 1] < nums[i] + nums[j]) {
+                    k++;
+                }
+                //这区间的都是正确值
+                ans += Math.max(k - j, 0);
+            }
+        }
+
+        return ans;
+
     }
+
+}
