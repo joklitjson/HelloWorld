d314a38be1ff2b7502a6834e2ead6199bf10e098
wenlong.chen <wenlong.chen@weimob.com>
2024-11-15 15:05:48 +0800

    update

--- myPratices/src/backtrack/Subsets.java
+++ myPratices/src/backtrack/Subsets.java
@@ -12,34 +12,36 @@ public class Subsets {
 //            System.out.println(Arrays.toString(element.toArray()));
 //        }
 
-        combine(3,3);
+//        combine(3, 3);
+//        for (List<Integer> element : res) {
+//            System.out.println(Arrays.toString(element.toArray()));
+//        }
+        combinationSum2(new int[]{2,3,6,7},7);
+
         for (List<Integer> element:res){
             System.out.println(Arrays.toString(element.toArray()));
         }
-//        combinationSum2(new int[]{10,1,2,7,6,1,5},8);
-//
-//        for (List<Integer> element:res){
-//            System.out.println(Arrays.toString(element.toArray()));
-//        }
     }
-   static List<List<Integer>> res=new ArrayList<>();
-    // 求子集问题
-    public static List<List<Integer>> subsets(int[] nums){
 
-        backtrack(nums,0,new ArrayList<>());
+    static List<List<Integer>> res = new ArrayList<>();
+
+    // 求子集问题
+    public static List<List<Integer>> subsets(int[] nums) {
+
+        backtrack(nums, 0, new ArrayList<>());
         return res;
     }
 
     //可以使用二进制 实现，每个数字是否选择，有两种情况，因此就有n^2次不同的结果
     public static List<List<Integer>> subsets2(int[] nums) {
         res.clear();
-        int n=nums.length;
-        List<Integer> tmp=new ArrayList<>();
-        for (int mask=0;mask<(1<<n);mask++){
+        int n = nums.length;
+        List<Integer> tmp = new ArrayList<>();
+        for (int mask = 0; mask < (1 << n); mask++) {
             //计算位数是1的数 有哪几个
-            for (int i=0;i<nums.length;i++){
-                if ((mask&(1<<i))==1){
-                    tmp.add(nums[i] );
+            for (int i = 0; i < nums.length; i++) {
+                if ((mask & (1 << i)) == 1) {
+                    tmp.add(nums[i]);
                 }
             }
             //一次结果
@@ -48,10 +50,10 @@ public class Subsets {
         return res;
     }
 
-    private static void backtrack(int[] nums,int start,List<Integer> trace){
+    private static void backtrack(int[] nums, int start, List<Integer> trace) {
         res.add(new ArrayList<>(trace));
 
-        for (int i=start;i<nums.length;i++){
+        for (int i = start; i < nums.length; i++) {
             // 剪枝逻辑，值相同的相邻树枝，只遍历第一条
             if (i > start && nums[i] == nums[i - 1]) {
                 continue;
@@ -59,34 +61,34 @@ public class Subsets {
             //选择
             trace.add(nums[i]);
             // 通过 start 参数控制树枝的遍历，避免产生重复的子集
-            backtrack(nums,i+1,trace);
+            backtrack(nums, i + 1, trace);
             //撤销
-            trace.remove(trace.size()-1);
+            trace.remove(trace.size() - 1);
         }
     }
 
     //组合问题：选择k个数字
-   static List<List<Integer>> combine(int n, int k){
-        backtrackCombine(n,1,k,new ArrayList<>());
+    static List<List<Integer>> combine(int n, int k) {
+        backtrackCombine(n, 1, k, new ArrayList<>());
 
         return res;
     }
 
     //在子集的基础上进行判断路径的长度是否等于k即可
-    private static void backtrackCombine(int n,int start,int k,List<Integer> trace){
+    private static void backtrackCombine(int n, int start, int k, List<Integer> trace) {
         //
-        if (trace.size()==k){
+        if (trace.size() == k) {
             res.add(new ArrayList<>(trace));
             return;
         }
 
-        for (int i=start;i<=n;i++) {
+        for (int i = start; i <= n; i++) {
 
             trace.add(i);
 
 //            backtrackCombine(n, i + 1, k, trace);
             //可重复选择
-            backtrackCombine(n, i , k, trace);
+            backtrackCombine(n, i, k, trace);
 
 
             trace.remove(trace.size() - 1);
@@ -95,6 +97,7 @@ public class Subsets {
 
     /**
      * 目标和为target的组合，可能存在相同元素
+     *
      * @param candidates
      * @param target
      * @return
@@ -102,32 +105,32 @@ public class Subsets {
     public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
 
         Arrays.sort(candidates);
-        combinationSum2Backtrace(candidates,target,0,0,new ArrayList<>());
+        combinationSum2Backtrace(candidates, target, 0, 0, new ArrayList<>());
         return res;
     }
 
-    private static void combinationSum2Backtrace(int[] candidates, int target,int start,int traceSum,List<Integer> trace){
-        if (traceSum==target){
+    private static void combinationSum2Backtrace(int[] candidates, int target, int start, int traceSum, List<Integer> trace) {
+        if (traceSum == target) {
             res.add(new ArrayList<>(trace));
         }
 
-        for (int i=start;i<candidates.length;i++){
-            //相同元素 则跳过
-            if (i>start&&candidates[i]==candidates[i-1]){
+        for (int i = start; i < candidates.length; i++) {
+            //相同元素 则跳过:当前元素和前一个元素相同则跳过（前提条件需要先排序）
+            if (i > start && candidates[i] == candidates[i - 1]) {
                 continue;
             }
-            if (candidates[i]+traceSum>target){
+            //提交进行剪枝
+            if (candidates[i] + traceSum > target) {
                 continue;
             }
             trace.add(candidates[i]);
-            traceSum+=candidates[i];
+            traceSum += candidates[i];
 
-            combinationSum2Backtrace(candidates,target,i+1,traceSum,trace);
-
-            trace.remove(trace.size()-1);
-            traceSum-=candidates[i];
-
+            //注意：如果可以重复选择，则下一个元素的位置 还是从i开始，如果不能重复选择，则下一个元素的位置从i+1开始
+            combinationSum2Backtrace(candidates, target, i+1 , traceSum, trace);
+            trace.remove(trace.size() - 1);
+            traceSum -= candidates[i];
         }
     }
-    }
+}
 
