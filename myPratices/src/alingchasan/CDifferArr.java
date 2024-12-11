79345bb4b85cabb929bb3b178974105ea4ebd4fb
wenlong.chen <wenlong.chen@weimob.com>
2024-12-11 09:20:25 +0800

    1

---
+++ myPratices/src/alingchasan/CDifferArr.java
@@ -0,0 +1,4 @@
+package alingchasan;
+
+public class CDifferArr {
+}
--- myPratices/src/binary/BinarySearchTest.java
+++ myPratices/src/binary/BinarySearchTest.java
@@ -6,7 +6,10 @@ import java.util.Map;
 public class BinarySearchTest {
 
     public static void main(String[] args) {
+//        System.out.println(new BinarySearchTest().maximumCandies(new int[]{1,2,3,4,10},5));
 
+//        System.out.println(new BinarySearchTest().minimumTime(new int[]{1,2,3},5));
+        System.out.println(new BinarySearchTest().minimizeArrayValue(new int[]{3,7,1,6}));
     }
     /**
      * 查找元素的位置：如果不存在则返回应该插入的位置
@@ -112,5 +115,164 @@ public class BinarySearchTest {
             map.put(value, value);
         }
         return new int[0];
+    }
+
+
+    /**
+     * 2226. 每个小孩最多能分到多少糖果
+     * 方案：先计算糖果的总数量，如果sum()<k 则不能分给k个小朋友
+     * 然后在使用二分法进行 试探：，此类问题的模板就是定义一个check方法，判断当前的target是否满足，然后在进行 +1、-1缩进
+     * @param candies
+     * @param k
+     * @return
+     */
+    public int maximumCandies(int[] candies, long k) {
+        long sum = 0;
+        for (int value : candies) {
+            sum += value;
+        }
+        if (sum < k) {
+            return 0;
+        }
+
+        //定义上下边界，然后在使用二分去check
+        long left = 1, right =  (sum );
+
+        long ans = 0;
+
+        while (left <= right) {
+            long middle = left + (right - left) / 2;
+            if (check(candies, k, middle)) {
+                //表示可以分成middle堆，因此我们在试探这 看看能否在进行分更大的堆
+                ans = middle;
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return (int) ans;
+    }
+
+    /**
+     * 判断这堆糖果 是否能分给k个孩子，每人num个糖果
+     * @param candies
+     * @param k
+     * @param limit
+     * @return
+     */
+    private boolean check(int[] candies, long k ,long limit) {
+        for (int value : candies) {
+            if (value < limit) {
+                //当前糖果不能分成一堆
+                continue;
+            } else if (value == limit) {
+                k--;
+            } else {
+                //value/num 表示可以分的堆数
+                k -= value / limit;
+            }
+        }
+        return k <= 0;
+    }
+
+
+    /**
+     * 2187. 完成旅途的最少时间
+     * @param time
+     * @param totalTrips
+     * @return
+     */
+    public long minimumTime(int[] time, int totalTrips) {
+
+        int max = time[0], min = time[0];
+        for (int val : time) {
+            max = Math.max(max, val);
+            min = Math.min(min, val);
+        }
+
+//        long left = 1, right =Math.max(Long.MAX_VALUE,max * totalTrips) ;
+
+
+        //大大数的情况下可以通过
+        int avg = (totalTrips - 1) / time.length + 1;
+        // 循环不变量：check(left) 恒为 false
+        long left = (long) min * avg - 1;
+        // 循环不变量：check(right) 恒为 true
+        long right = Math.min((long) max * avg, (long) min * totalTrips);
+
+        long ans = 0;
+        while (left <= right) {
+            long middleTime = left + (right - left) / 2;
+            if (checkComplete(time, totalTrips, middleTime)) {
+                //这个时间可以完成，在求更小的时间
+                ans = middleTime;
+                right = middleTime - 1;
+            } else {
+                left = middleTime + 1;
+            }
+        }
+        return ans;
+    }
+    private boolean checkComplete(int[] time, int totalTrips,long totalTime){
+        //判断在totalTime 时间内，每个车子完成了多少次旅行
+        long cnt = 0;
+        for (int val:time){
+            cnt+=totalTime/val;
+        }
+        return cnt>=totalTrips;
+    }
+
+    /**
+     * 2439. 最小化数组中的最大值
+     * 二分查找
+     * @param nums
+     * @return
+     */
+    public int minimizeArrayValue(int[] nums) {
+        int max=nums[0],min=nums[0];
+        for(int val:nums){
+            max=Math.max(max,val);
+            min=Math.min(min,val);
+        }
+// 初始化二分查找的左右边界。
+        // left 初始化为 -1 表示还没有任何有效值。
+        // right 初始化为数组中最大的元素值。
+        int left=min,right=max;
+        int ans=0;
+        while (left<=right){
+            int middle=left+(right-left)/2;
+            //判断调整后的数组的值是否都不超过 middle
+            if (check(nums,middle)){
+                ans=middle;
+                right=middle-1;
+            }
+            else {
+                left=middle+1;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     *  // 辅助函数 check 检查是否可以通过调整使数组的最大值不超过 limit。
+     * @param nums
+     * @param limit
+     * @return
+     */
+    private boolean check(int nums[],int limit){
+        long exact=0;//倒序遍历，表示可以提供给前面的元素数量，不包含第一个(使用long 是为了防止数很大 溢出了)
+        for (int i= nums.length-1;i>0;i--){
+            //表示当前元素使用了之前元素贡献的数 还是超过了limit，为了不超过limit，则把超过的值贡献给下一个元素
+            if (nums[i]+exact>limit){
+                exact=nums[i]+exact-limit;
+            }
+            else {
+                //当前元素加上之前元素贡献的exact 都小于 limit，那么之前的exact都给当前元素使用了，索引把exact设置成0
+                exact=0;
+            }
+        }
+
+        //判断第一个数是否不超过limit
+        return nums[0]+exact<=limit;
     }
 }
