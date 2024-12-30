81bcbb83dae211a4af56537ecdf685d4f4d77338
wenlong.chen <wenlong.chen@weimob.com>
2024-12-30 18:30:05 +0800

    替换数组中非互质的数

--- myPratices/src/alingchasan/DStack.java
+++ myPratices/src/alingchasan/DStack.java
@@ -1,7 +1,12 @@
 package alingchasan;
 
+import org.omg.CORBA.INTERNAL;
+
 import java.util.*;
 
+/**
+ * 栈题目：可以使用栈进行模拟、也可以使用数组、StringBuilder进行模拟
+ */
 public class DStack {
 
     public static void main(String[] args) {
@@ -42,7 +47,15 @@ public class DStack {
 
         System.out.println(dinnerPlates.pop());
         System.out.println(dinnerPlates.pop());
+
+//        System.out.println(isValid("aabcbc"));
+
+        System.out.println(removeDuplicates("deeedbbcccbdaa",3));
+        System.out.println("countCollisions=="+countCollisions("SSRSSRLLRSLLRSRSSRLRRRRLLRRLSSRR"));
+
+        System.out.println("asteroidCollision=="+Arrays.toString(asteroidCollision(new int[]{10,2,-5})));
     }
+
 
     /**
      * 1441. 用栈操作构建数组
@@ -417,6 +430,45 @@ public class DStack {
         }
     }
 
+
+    /**
+     * 2434. 使用机器人打印字典序最小的字符串
+     * 解决方案：使用栈+贪心，如果某个字符都比后面的字符小，则可以进行打印了，因为后面找不到比他在小的字符了
+     * 先计算每个字符的个数，然后遍历的同时 在计算第一个个数不为0的字符，比较他们大小，如果大于当前字符则可以从这里打印了
+     * @param s
+     * @return
+     */
+    public String robotWithString(String s) {
+
+        int cnt[] = new int[26];
+        Stack<Character> stack = new Stack<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        //统计个数
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i) - 'a']++;
+        }
+
+        int minIdx = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i) - 'a']--;
+
+            //找到第一个不为0的字符，表示的是当前字符后面的最小字符
+            while (minIdx <25 && minIdx == 0) {
+                minIdx++;
+            }
+
+            stack.push(s.charAt(i));
+
+            //当前栈顶字符 小于后续字符
+            while (!stack.isEmpty() && stack.peek() - 'a' <= minIdx) {
+                stringBuilder.append(stack.pop());
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
     /**
      * 2589. 完成所有任务的最少时间
      * 贪心+排序
@@ -476,6 +528,257 @@ public class DStack {
         }
         return stack.size();
     }
+
+    /**
+     * 1047. 删除字符串中的所有相邻重复项
+     * 方案：使用栈  比较栈顶元素和当前元素，此处可以使用模拟栈
+     * @param s
+     * @return
+     */
+    public String removeDuplicates(String s) {
+        StringBuilder stringBuilder=new StringBuilder();
+        for (int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            //相同则移除
+            if (stringBuilder.length()>0&&stringBuilder.charAt(stringBuilder.length()-1)==c){
+                stringBuilder.deleteCharAt(stringBuilder.length()-1);
+            }
+            else {
+                stringBuilder.append(c);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+
+    /**
+     * 整理字符串：1544. 整理字符串
+     * 其实就是删除 相邻的一大一小的字符
+     * @param s
+     * @return
+     */
+    public String makeGood(String s) {
+        StringBuilder stringBuilder=new StringBuilder();
+        for (int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            //相同则移除
+            if (stringBuilder.length()>0&&stringBuilder.charAt(stringBuilder.length()-1)!=c
+            &&Character.toLowerCase(stringBuilder.charAt(stringBuilder.length()-1))==Character.toLowerCase(c)){
+                stringBuilder.deleteCharAt(stringBuilder.length()-1);
+            }
+            else {
+                stringBuilder.append(c);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 1003. 检查替换后的词是否有效
+     * 愿意：空字符 "" 通过插入 字符 n、个 abd 变成了目标字符s，判断字符s是否是插入了n个字符abc 变成的，
+     * 我们可以反过来思考：字符s 是否可以通过删除 临近的abc，然后变成空字符
+     * @param s
+     * @return
+     */
+    public  static boolean isValid(String s) {
+
+        //使用数组模拟栈
+        char [] cc=new char[s.length()];
+        int topIdx=-1;
+        for (int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if (topIdx-1>=0){
+                //判断最近三个字符是否相等？如何判断n个字符呢？使用一个循环就行了
+                if (c=='c'&&cc[topIdx]=='b'&&cc[topIdx-1]=='a'){
+                    topIdx=topIdx-2;
+                }
+                else {
+                    cc[++topIdx]=c;
+                }
+            }
+            else {
+                cc[++topIdx]=c;
+            }
+        }
+        //判断数组是否为空
+        return topIdx==-1;
+    }
+
+    /**
+     * 1209. 删除字符串中的所有相邻重复项 II
+     * 使用模拟栈的方式 遍历字符，然后当 已经遍历的长度大于k时，我们在判断长度k的字符串是否都相等
+     * @param s
+     * @param k
+     * @return
+     */
+    public static String removeDuplicates(String s, int k) {
+        int idx=-1;
+        char [] cc=new char[s.length()];
+        for (int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            cc[++idx]=c;
+            if (idx>=k-1){
+                int j=idx-k+1;//判断[j,idx]之间的元素
+                //从idx 往前判断k个字符是否相等
+                while (j<idx&&cc[j]==c){
+                    j++;
+                }
+                //删除这几个字符
+                if (j==idx){
+                    //重置游标
+                    idx=idx-k;
+                }
+            }
+        }
+
+        if (idx==-1){
+            return "";
+        }
+        return new String(cc,0,idx+1);
+    }
+
+
+    /**
+     * 2211. 统计道路上的碰撞次数
+     * 使用栈：分情况讨论
+     * @param directions
+     * @return
+     */
+    public static int countCollisions(String directions) {
+
+        int ans = 0;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < directions.length(); i++) {
+            char c = directions.charAt(i);
+
+            if (c == 'L') {
+                if (stack.isEmpty()) {
+                    continue;//肯定不会碰撞，不用添加了
+                }
+                if (stack.peek() == 'S') {
+                    ans++;
+//                        stack.push('S');//加不加都行，因为此时栈顶已经存在了stop 远山
+                } else if (stack.peek() == 'R') {
+                    ans++;//当前会加1,下面的第一个while 循环也会+1
+                    while (!stack.isEmpty() && stack.peek() == 'R') {
+                        stack.pop();
+                        ans++;
+                    }
+                    //碰撞之后就停止了
+                    stack.push('S');
+                } else {
+                    //左边是向左的，没关系可以继续添加
+//                        stack.push(c);
+                }
+            } else if (c == 'S') {
+                while (!stack.isEmpty() && stack.peek() == 'R') {
+                    stack.pop();
+                    ans++;
+                }
+                stack.push(c);
+            } else if (c == 'R') {
+                stack.push(c);
+            }
+        }
+        return ans;
+    }
+
+
+    /**
+     * 735. 小行星碰撞
+     * 使用模拟栈：向向则爆炸，保留质量大的，如果两个质量相同则 都爆炸
+     * @param asteroids
+     * @return
+     */
+    public static int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int aster : asteroids) {
+            if (stack.isEmpty()) {
+                stack.push(aster);
+            } else {
+                //相向而行
+                if (stack.peek() > 0 && aster < 0) {
+                    boolean needPush=true;//判断当前的星星是否需要加入
+                    while (!stack.isEmpty()&&stack.peek() > 0 && aster < 0) {
+                        if (stack.peek() == Math.abs(aster)) {
+                            stack.pop();
+                            needPush=false;
+                            break;
+                        } else if (stack.peek() < Math.abs(aster)) {
+                            //把栈顶的星星 消灭
+                            stack.pop();
+                        } else {
+                            //当前元素较小,不需要入栈：消灭当前星星
+                            needPush=false;
+                            break;
+                        }
+                    }
+                    if (needPush){
+                        stack.push(aster);
+                    }
+                } else {
+                    //他俩方向不会碰撞
+                    stack.push(aster);
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return new int[0];
+        }
+        //输出答案
+        int[] ans = new int[stack.size()];
+        int idx = ans.length - 1;
+        while (idx >= 0 && !stack.isEmpty()) {
+            ans[idx--] = stack.pop();
+        }
+        return ans;
+    }
+
+    /**
+     * 2197. 替换数组中的非互质数
+     * 使用栈处理
+     * @param nums
+     * @return
+     */
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int num : nums) {
+            ans.add(num);
+            while (ans.size() >1) {
+                //判断这两个数是否互质:
+                int num1 = ans.get(ans.size() - 1);
+                int num2 = ans.get(ans.size() - 2);
+                int g = gcd(num1, num2);
+                if (g != 1) {
+                    //非互质的情况下 删除他们俩，然后在加入他俩的最小公倍数
+                    ans.remove(ans.size() - 1);
+                    ans.remove(ans.size() - 1);
+                    ans.add(num1 / g * num2);
+                } else {
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    /**
+     * 2216. 美化数组的最少删除数
+     * @param nums
+     * @return
+     */
+    public int minDeletion(int[] nums) {
+        return 0;
+    }
+
     /**
      * 1172. 餐盘栈
      * 解决方案：记录pop的下标，在记录左侧非空栈的下标，
@@ -494,7 +797,7 @@ public class DStack {
             if (!notFullStackIdx.isEmpty()){
                 int idx= notFullStackIdx.first();
                 map.get(idx).add(val);
-                //移除
+                //移除;
                 if (map.get(idx).size()==cap){
                     notFullStackIdx.pollFirst();
                 }
