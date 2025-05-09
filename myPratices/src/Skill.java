977589c7def1638df7ad2569719bb166517df625
wenlong.chen <wenlong.chen@weimob.com>
2025-05-09 17:27:17 +0800

    1

--- myPratices/src/Skill.java
+++ myPratices/src/Skill.java
@@ -1,5 +1,6 @@
 /**
  * 技巧：遍历二维数组，有时候需要设置vis数组记录是否被访问，可以使用原数组，把值设置成负数、0之类的表示已经被访问了
+ * 技巧二：求一个数组中所有元素左边和右边的最大值，只需要正序遍历一次，在倒序遍历一次即可
  */
 public class Skill {
 
@@ -26,5 +27,30 @@ public class Skill {
 
         System.out.println("第一大的值: " + firstMax);
         System.out.println("第二大的值: " + secondMax);
+    }
+
+    /**
+     * 求一个数组的左边和右边的最大值
+     * @param arr
+     * @return
+     */
+    public static int[][] calculateMax(int[] arr) {
+        int n = arr.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        // 计算左边最大值数组
+        leftMax[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], arr[i]);
+        }
+
+        // 计算右边最大值数组
+        rightMax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], arr[i]);
+        }
+
+        return new int[][]{leftMax, rightMax};
     }
 }
