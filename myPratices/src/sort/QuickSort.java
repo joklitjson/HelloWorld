31a628c85d46f9c8cc2e2141264642ca7aaf7552
wenlong.chen <wenlong.chen@weimob.com>
2025-05-15 14:19:39 +0800

    快速排序总结

--- myPratices/src/sort/QuickSort.java
+++ myPratices/src/sort/QuickSort.java
@@ -2,13 +2,22 @@ package sort;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.Stack;
 
+/**
+ *
+ * 不同的基准值方法，对排序的时间性能有决定性影响。下面介绍两种常用的分区法。
+ *Lomuto 分区法：此方案选择最后一个元素作为基数，由于此方案代码更 紧凑 且 易于理解，常被用于教程。但整体效率低于 Hoare 的原始方案。特别是在整个数组已经有序时，效率下降到O(n^2)O(n
+ *
+ * Hoare 分区法 :使用数组的中间元素作为基准值，Hoare 的方案比 Lomuto 的分区方案更有效，使用双向指针，分别查找一个 大于基准索引和一个小于基准索引，然后在交换
+ */
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arr=new int[]{1, 31, 4, 1, 2, 5, 7, 8,};
+        int[] arr=new int[]{1, 31, 4, 6, 2, 5, 7, 8,};
 //        quickSort(arr,0,arr.length-1);
-        quickSortWithStack(arr,0,arr.length-1);
+//        quickSortWithStack(arr,0,arr.length-1);
+        randomizedQuicksort(arr,0,arr.length-1);;
         for (int value : arr) {
             System.out.print(value + " ");
         }
@@ -132,5 +141,67 @@ public class QuickSort {
         // partionValue放在合适的位置
         arr[left]=partionValue;
         return left;
+    }
+
+
+    // 随机快速排序主函数
+    public static void randomizedQuicksort(int[] arr, int low, int high) {
+        if (low < high) {
+            // 随机选择基准元素并分区
+            int pivotIndex = randomizedPartition(arr, low, high);
+            // 递归排序左右两部分
+            randomizedQuicksort(arr, low, pivotIndex - 1);
+            randomizedQuicksort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    /**
+     * 随机快速排序：就是随机选择基准值，为了更方便的把数组均分
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    private  static int randomizedPartition(int [] arr,int left,int right){
+
+        Random random=new Random();
+        int randomIdx=random.nextInt(right-left+1)+left;
+        //交换元素：把随机选择的元素交换到数组末尾或者开头，然后在使用 交换、填坑的方式进行计算
+        swap(arr,right,randomIdx);
+
+        // 使用标准分区方法
+        return partition3(arr,left,right);
+    }
+
+    /**
+     * Lomuto分区方案 核心思想就是把小于 pivot的值都交换到他左边，然后交换完成后，左边的值都小于他了，那么他就放在 左边指针的下一个位置(i+1)
+     * @param arr
+     * @param low
+     * @param high
+     * @return
+     */
+    // 标准分区方法（Lomuto分区方案）
+    private static int partition3(int[] arr, int low, int high) {
+        // 基准元素是数组末尾的元素
+        int pivot = arr[high];
+        int i = low - 1;
+
+        // 将小于等于基准的元素交换到左边
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        // 将基准元素放到正确的位置
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int [] arr,int i,int j){
+        int tmp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
     }
 }
